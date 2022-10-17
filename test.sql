5f68fe1dea87939f386155d89729b23d02a3880b
Shreya-Shashidhara <94812157+Shreya-Shashidhara@users.noreply.github.com>
2022-10-17 12:07:54 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -17,13 +17,32 @@ insert into Worker values('005','Vivek','Bhati',500000,'2014-06-11 09:00:00','Ad
 insert into Worker values('006','Vipul','Diwan',200000,'2014-06-11 09:00:00','Account');
 insert into Worker values('007','Satish','Kumar',75000,'2014-01-20 09:00:00','Account');
 insert into Worker values('008','Geetika','Chauhan',90000,'2014-04-11 09:00:00','Admin');
-select First_Name,replace(First_name,'a','A') from test.Worker;
+-- Query 1 
+select First_Name,replace(First_name,'a','A') from test.Worker; 
+
+-- Query 2
 select concat(First_Name, ' ',Last_Name) AS Complete_Name from test.Worker;
+
+-- Query 3
 select* from test.Worker Order By First_Name,Department DESC;
+
+-- Query 4
 select * from test.Worker where Department ='Admin';
+
+-- Query 5
 select * from test.Worker where First_Name IN ('Vipul','Satish');
+
+-- Query 6
 select * from test.Worker where First_Name like '%a';
+
+-- Query 7
 select * from test.Worker where First_Name like '______h';
+
+-- Query 8
 select First_Name as Worker_Name from test.Worker;
+
+-- Query 9
 select First_Name,upper(First_Name) from test.Worker;
+
+-- Query 10
 select * from test.Worker where Salary between 100000 and 500000;
